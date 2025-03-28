01b93cc1e7098d102506b916999d73ad7a572b7f
Tridib Roy Arjo <softmetricltd@gmail.com>
2025-03-29 00:16:03 +0600

    Nothing

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -3,7 +3,6 @@ import SiteHeader from "@/components/site-header";
 import { siteConfig } from "@/config/site";
 import Link from "next/link";
 import { ThemeProvider } from "./theme-provider";
-import ChatwootWidget from "./integrations/chatwoot";
 
 export default function App({ children }: PropsWithChildren) {
   return (
@@ -30,7 +29,6 @@ export default function App({ children }: PropsWithChildren) {
           </p>
         </footer>
       </div>
-      <ChatwootWidget />
     </ThemeProvider>
   );
 }

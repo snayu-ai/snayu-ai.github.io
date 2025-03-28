a71ce5464e98a104e6c3924e24977676e632f568
Tridib Roy Arjo <softmetricltd@gmail.com>
2025-03-29 00:26:51 +0600

    fixed grids

--- src/components/ui/ripple-card.tsx
+++ src/components/ui/ripple-card.tsx
@@ -1,6 +1,7 @@
 import Ripple from "@/components/magicui/ripple";
+import { ReactNode } from "react";
 
-export function RippleCard({text}: {text: string}) {
+export function RippleCard({text}: {text: ReactNode}) {
   return (
     <div className="relative flex h-full w-full flex-col items-center justify-center overflow-hidden rounded-lg border bg-background md:shadow-xl">
       <p className="z-10 whitespace-pre-wrap text-center text-5xl font-medium tracking-tighter text-card-foreground p-5 select-none cursor-default">
